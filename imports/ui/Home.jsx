2ac1374413dba548afd98d05211f10e2862a25fc
Josh English <josh@joshenglish.com>
2016-06-05 23:11:11 -0500

    Currently the Home is not displaying data from the Server due to the additional API call

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 
 export default React.createClass({
+  getInitialState: () => {
+    return { services: [] }
+  },
+  componentDidMount: () => {
+    var self = this;
+    Meteor.call('product.get', (err, data) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(data);
+      }
+    })
+  },
   render() {
     return <div>Home</div>
   }
--- server/main.js
+++ server/main.js
@@ -1,1 +1,2 @@
 import './admin.js'
+import './home.js'
