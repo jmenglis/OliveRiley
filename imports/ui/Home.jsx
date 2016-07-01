c5cc8d3a0088cd26d92ce37655484e1275497f39
Josh English <josh@joshenglish.com>
2016-07-01 14:13:37 -0500

    Added Masonry to front-end.  Homepage was divided up to contain the proper information

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -1,14 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
+import Masonry from 'react-masonry-component';
 
 var IndividualProduct = React.createClass({
   render() {
     return (
-      <ul>
+      <ul class="product-image-element">
+        <li><a href={`/products/${this.props.data.slug}`}><img width="180" height="270" src={this.props.data.images[0].url.http}/></a></li>
+        <li><strong><a href={`/products/${this.props.data.slug}`}>{this.props.data.brand.value.toUpperCase()}</a></strong></li>
         <li><a href={`/products/${this.props.data.slug}`}>{this.props.data.title}</a></li>
-        <li><a href={`/products/${this.props.data.slug}`}><img src={this.props.data.images[0].url.http}/></a></li>
-        <li>Price: {this.props.data.price.data.formatted.without_tax}</li>
+        <li>{this.props.data.price.data.formatted.without_tax}</li>
       </ul>
     )
   }
@@ -26,11 +28,24 @@ export default React.createClass({
     })
   },
   render() {
+    var masonryOptions = {
+      columnWidth: 200,
+      isFitWidth: true,
+      gutter: 100
+    }
     return (
       <div>
+        <Masonry
+          className={'product-grid'}
+          elementType={'div'}
+          options={masonryOptions}
+        >
         {this.state.products.map((prod, i) => {
-          return <IndividualProduct key={i} data={prod} />
+          return (
+            <IndividualProduct key={i} data={prod} />
+          )
         })}
+        </Masonry>
       </div>
     )
   }
