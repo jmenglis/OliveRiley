2fe796b0bfa33553420c749697312bf491f1a926
Josh English <josh@joshenglish.com>
2016-11-09 19:29:20 -0600

    Corrected incorrect code syntax in login file

--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -1,8 +1,8 @@
-import agent from 'superagent'
+import agent from 'superagent';
 
 const moltin = require('moltin')({
   publicId: process.env.MOLTIN_CLIENTID,
-  secretKey: process.env.MOLTIN_CLIENTSECRET
+  secretKey: process.env.MOLTIN_CLIENTSECRET,
 });
 
 module.exports = [
@@ -10,19 +10,19 @@ module.exports = [
     method: 'POST',
     path: '/api/login',
     handler: (request, reply) => {
-      moltin.Authenticate(() => {
+      moltin.Authenticate(() => { // eslint-disable-line
         agent
-          .post('https://api.molt.in/v1/customers/token?email=' + request.payload.email + '&password=' + request.payload.password)
-          .set('Authorization','Bearer ' + moltin.options.auth.token)
+          .post(`https://api.molt.in/v1/customers/token?email=${request.payload.email}&password=${request.payload.password}`)
+          .set('Authorization', `Bearer ${moltin.options.auth.token}`)
           .end((err, res) => {
-            request.yar.set('user',{
+            request.yar.set('user', {
               id: res.body.result.id,
               email: res.body.result.email,
               token: res.body.result.token,
             });
-            reply({loggedIn: true});
-          })
+            reply({ loggedIn: true });
+          });
       });
-    }
+    },
   },
 ];
